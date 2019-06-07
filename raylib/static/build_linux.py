519c182a5c8b1f9103fe22c4772e53494daca391
Electron Studio <github@electronstudio.co.uk>
2019-06-07 01:13:06 +0100

    another try at linux libs

--- raylib/static/build_linux.py
+++ raylib/static/build_linux.py
@@ -13,7 +13,7 @@ ffibuilder.set_source("_raylib_cffi",
                       """
                            #include "../raylib.h"   // the C header, installed in the system include dir we assume
                       """,
-                      extra_link_args=['../../linux_libs/core.o', '../../linux_libs/models.o', '../../linux_libs/raudio.o', '../../linux_libs/rglfw.o','../../linux_libs/shapes.o','../../linux_libs/text.o','../../linux_libs/textures.o','../../linux_libs/utils.o'],
+                      extra_link_args=['../../linux_libs/core.o', '../../linux_libs/models.o', '../../linux_libs/raudio.o', '../../linux_libs/rglfw.o','../../linux_libs/shapes.o','../../linux_libs/text.o','../../linux_libs/textures.o','../../linux_libs/utils.o','-lm', '-lpthread', '-lGLU', '-lGL',  '-lrt', '-lm', '-ldl', '-lX11', '-lpthread'],
                       libraries=['GL','m','pthread', 'dl', 'rt', 'X11']
                       )
 
