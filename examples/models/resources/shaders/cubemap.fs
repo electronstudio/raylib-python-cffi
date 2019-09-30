95b31c40decc5fef0dff6c4c8d32d426c0e1c7b6
Pebaz <pebazium@gmail.com>
2019-09-30 06:51:49 -0400

    Add Skybox Example (#5)
    * added skybox example
    
    * use existing resource directory

--- examples/models/resources/shaders/cubemap.fs
+++ examples/models/resources/shaders/cubemap.fs
@@ -8,31 +8,31 @@
 
 #version 330
 
-# Input vertex attributes (from vertex shader)
-in vec3 fragPos
+// Input vertex attributes (from vertex shader)
+in vec3 fragPosition;
 
-# Input uniform values
-uniform sampler2D equirectangularMap
+// Input uniform values
+uniform sampler2D equirectangularMap;
 
-# Output fragment color
-out vec4 finalColor
+// Output fragment color
+out vec4 finalColor;
 
 vec2 SampleSphericalMap(vec3 v)
-[
-    vec2 uv = vec2(atan(v.z, v.x), asin(v.y))
-    uv *= vec2(0.1591, 0.3183)
-    uv += 0.5
-    return uv
-]
+{
+    vec2 uv = vec2(atan(v.z, v.x), asin(v.y));
+    uv *= vec2(0.1591, 0.3183);
+    uv += 0.5;
+    return uv;
+}
 
 void main()
-[
-    # Normalize local position
-    vec2 uv = SampleSphericalMap(normalize(fragPos))
+{
+    // Normalize local position 
+    vec2 uv = SampleSphericalMap(normalize(fragPosition));
 
-    # Fetch color from texture map
-    vec3 color = texture(equirectangularMap, uv).rgb
+    // Fetch color from texture map
+    vec3 color = texture(equirectangularMap, uv).rgb;
 
-    # Calculate final fragment color
-    finalColor = vec4(color, 1.0)
-]
+    // Calculate final fragment color
+    finalColor = vec4(color, 1.0);
+}
--- examples/models/resources/shaders/cubemap.vs
+++ examples/models/resources/shaders/cubemap.vs
@@ -8,21 +8,21 @@
 
 #version 330
 
-# Input vertex attributes
-in vec3 vertexPosition
+// Input vertex attributes
+in vec3 vertexPosition;
 
-# Input uniform values
-uniform mat4 projection
-uniform mat4 view
+// Input uniform values
+uniform mat4 projection;
+uniform mat4 view;
 
-# Output vertex attributes (to fragment shader)
-out vec3 fragPos
+// Output vertex attributes (to fragment shader)
+out vec3 fragPosition;
 
 void main()
-[
-    # Calculate fragment position based on model transformations
-    fragPos = vertexPosition
+{
+    // Calculate fragment position based on model transformations
+    fragPosition = vertexPosition;
 
-    # Calculate final vertex position
-    gl_Position = projection*view*vec4(vertexPosition, 1.0)
-]
+    // Calculate final vertex position
+    gl_Position = projection*view*vec4(vertexPosition, 1.0);
+}
