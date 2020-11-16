5d5c38a154673ff2ad42624f0f30de3c75dcbcae
osboxes.org <git@electronstudio.co.uk>
2020-11-16 01:47:44 -0500

    update linux binaries to raylib 3.1-dev master and include python 3.9

--- raylib/static/build_linux_multi.sh
+++ raylib/static/build_linux_multi.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 rm *linux-gnu.so
-pyenv global 3.8.1
+pyenv install -s 3.9.0
+pyenv global 3.9.0
 pip3 install cffi
 python build_linux.py
-pyenv global 3.7.6
+pyenv install -s 3.8.6
+pyenv global 3.8.6
 pip3 install cffi
 python build_linux.py
-pyenv global 3.6.10
+pyenv install -s 3.7.9
+pyenv global 3.7.9
+pip3 install cffi
+python build_linux.py
+pyenv install -s 3.6.12
+pyenv global 3.6.12
 pip3 install cffi
 python build_linux.py
