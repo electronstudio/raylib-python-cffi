677dfc0de0526879d3fff71ad17f36cd5762eb97
richard <github@fantastic.me.uk>
2021-06-12 20:03:28 +0100

    rpi build separate

---
+++ raylib/static/build_multi_rpi_nox.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+if ! command -v pyenv &> /dev/null
+then
+    echo "Pyenv required: https://github.com/pyenv/pyenv-installer"
+    exit
+fi
+
+pyenv update
+
+function build() {
+    echo "Building for Python $1"
+    pyenv install -s $1
+    pyenv global $1
+    pip3 install cffi
+    pip3 install wheel
+    rm -rf raylib/static/_raylib_cffi.* build
+    python setup.py bdist_wheel --plat-name manylinux2014_armv7l
+}
+
+build 3.9.5
+build 3.8.10
+build 3.7.10
+build 3.6.13
+
