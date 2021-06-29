fd6233e0d1cb0dbe6fcfefba755342582d95c135
electronstudio <misc@fantastic.me.uk>
2021-06-29 15:21:19 +0100

    mac build

--- raylib/static/build.py
+++ raylib/static/build.py
@@ -40,19 +40,13 @@ def build_mac():
     ffibuilder.set_source("raylib.static._raylib_cffi",
                           """
                                #include "../../raylib/raylib.h"   // the C header of the library, supplied by us here
-                          """
+                          """,
+                          extra_link_args=['raylib-c/src/libraylib.a', '-framework', 'OpenGL', '-framework', 'Cocoa', '-framework', 'IOKit', '-framework', 'CoreFoundation', '-framework', 'CoreVideo'],
                           )
-    # Hack to produce static linked lib using static librarylib.a supplied by us
-    version = sys.implementation.cache_tag
-    if version == 'cpython-36' or version == 'cpython-37':
-        version += 'm'
-    command = "clang -bundle -undefined dynamic_lookup ./_raylib_cffi.o -L/usr/local/lib -L/usr/local/opt/openssl/lib -L/usr/local/opt/sqlite/lib ../../libraylib_mac.a -F/System/Library/Frameworks -framework OpenGL -framework Cocoa -framework IOKit -framework CoreFoundation -framework CoreVideo -o ./_raylib_cffi."+version+"-darwin.so"
 
     if __name__ == "__main__":
         ffibuilder.compile(verbose=True)
-        if platform.system()=="Darwin":
-            print(command)
-            os.system(command)
+
 
 def build_rpi_nox():
     print("BUILDING FOR RASPBERRY PI")
