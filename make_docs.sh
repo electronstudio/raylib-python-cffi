5535d7d641c255e26a404ac4739bfc368e07c162
richard <github@fantastic.me.uk>
2021-10-08 05:36:20 +0100

    docs arent made by jekyll.

--- make_docs.sh
+++ make_docs.sh
@@ -4,5 +4,7 @@ python3 raylib/build.py
 pip3 install sphinx-autoapi myst_parser sphinx_rtd_theme
 python3 create_stub_pyray.py > pyray/__init__.pyi
 python3 create_stub_static.py >raylib/__init__.pyi
+rm -r docs
 cd docs-src
-make clean ; make html ; cp -a _build/html/. ../docs/
+make clean ; make html ; mv _build/html/ ../docs/
+touch ../docs/.nojekyll
