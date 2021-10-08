04dd13a3f31ce9a3df608e55af6cdcc1c1fcfdc2
richard <github@fantastic.me.uk>
2021-10-08 05:26:54 +0100

    small fix

--- make_docs.sh
+++ make_docs.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+python3 raylib/build.py
 pip3 install sphinx-autoapi myst_parser sphinx_rtd_theme
 python3 create_stub_pyray.py > pyray/__init__.pyi
 python3 create_stub_static.py >raylib/__init__.pyi
