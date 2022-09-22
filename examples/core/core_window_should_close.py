be5ff756b7ba41d039dbdc28ba799430f1b932b5
דור שפירא <Dor.Shapira@outlook.com>
2022-09-22 20:51:28 +0300

    make core_window_should_close.py stand for the conventions

--- examples/core/core_window_should_close.py
+++ examples/core/core_window_should_close.py
@@ -3,6 +3,9 @@
 raylib [core] example - Window Should Close
 
 """
+
+# Import
+# ------------------------------------------------------------------------------------
 from pyray import *
 from raylib.colors import (
     RAYWHITE,
@@ -16,59 +19,69 @@ from raylib import (
     KEY_Y,
     KEY_N
 )
+# ------------------------------------------------------------------------------------
 
-# Initialization
-# --------------------------------------------------------------------------------------
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 450
+# ------------------------------------------------------------------------------------
+# Program main entry point
+# ------------------------------------------------------------------------------------
+def main():
+    # Initialization
+    # --------------------------------------------------------------------------------------
+    SCREEN_WIDTH = 800
+    SCREEN_HEIGHT = 450
 
-init_window(SCREEN_WIDTH, SCREEN_HEIGHT, "raylib [core] example - window should close")
+    init_window(SCREEN_WIDTH, SCREEN_HEIGHT, "raylib [core] example - window should close")
 
-set_exit_key(KEY_NULL)  # Disable KEY_ESCAPE to close window, X-button still works
+    set_exit_key(KEY_NULL)  # Disable KEY_ESCAPE to close window, X-button still works
 
-exitWindowRequested = False  # Flag to request window to exit
-exitWindow = False  # Flag to set window to exit
+    exitWindowRequested = False  # Flag to request window to exit
+    exitWindow = False  # Flag to set window to exit
 
-set_target_fps(60)  # Set our game to run at 60 frames-per-second
-# --------------------------------------------------------------------------------------
+    set_target_fps(60)  # Set our game to run at 60 frames-per-second
+    # --------------------------------------------------------------------------------------
 
-# Main game loop
-while not exitWindow:
+    # Main game loop
+    while not exitWindow:
+        # Update
+        # ----------------------------------------------------------------------------------
+        #  Detect if X-button or KEY_ESCAPE have been pressed to close window
+        if window_should_close() or is_key_pressed(KEY_ESCAPE):
+            exitWindowRequested = True
 
-    # Update
+        if exitWindowRequested:
+
+            # A request for close window has been issued, we can save data before closing
+            # or just show a message asking for confirmation
+
+            if is_key_pressed(KEY_Y):
+                exitWindow = True
+            elif is_key_pressed(KEY_N):
+                exitWindowRequested = False
+        # ----------------------------------------------------------------------------------
+
+        # Draw
+        # ----------------------------------------------------------------------------------
+        begin_drawing()
+
+        clear_background(RAYWHITE)
+
+        if exitWindowRequested:
+
+            draw_rectangle(0, 100, SCREEN_WIDTH, 200, BLACK)
+            draw_text("Are you sure you want to exit program? [Y/N]", 40, 180, 30, WHITE)
+
+        else:
+            draw_text("Try to close the window to get confirmation message!", 120, 200, 20, LIGHTGRAY)
+
+        end_drawing()
+        # ----------------------------------------------------------------------------------
+
+    # De-Initialization
     # ----------------------------------------------------------------------------------
-    #  Detect if X-button or KEY_ESCAPE have been pressed to close window
-    if window_should_close() or is_key_pressed(KEY_ESCAPE):
-        exitWindowRequested = True
-
-    if exitWindowRequested:
-
-        # A request for close window has been issued, we can save data before closing
-        # or just show a message asking for confirmation
-
-        if is_key_pressed(KEY_Y):
-            exitWindow = True
-        elif is_key_pressed(KEY_N):
-            exitWindowRequested = False
-
+    close_window()  # Close window and OpenGL context
     # ----------------------------------------------------------------------------------
 
-    # Draw
-    # ----------------------------------------------------------------------------------
-    begin_drawing()
 
-    clear_background(RAYWHITE)
-
-    if exitWindowRequested:
-
-        draw_rectangle(0, 100, SCREEN_WIDTH, 200, BLACK)
-        draw_text("Are you sure you want to exit program? [Y/N]", 40, 180, 30, WHITE)
-
-    else:
-        draw_text("Try to close the window to get confirmation message!", 120, 200, 20, LIGHTGRAY)
-
-    end_drawing()
-    # ----------------------------------------------------------------------------------
-
-# De-Initialization
-close_window()  # Close window and OpenGL context
+# execute the main function
+if __name__ == '__main__':
+    main()
