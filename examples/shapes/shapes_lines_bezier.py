d123e25ef1b0de366c4e720bb497f78774baf86c
Dor Shapira <107134807+sDos280@users.noreply.github.com>
2022-09-22 21:23:02 +0300

    Update shapes_lines_bezier.py
    fixing typo

--- examples/shapes/shapes_lines_bezier.py
+++ examples/shapes/shapes_lines_bezier.py
@@ -60,6 +60,6 @@ def main():
     # ----------------------------------------------------------------------------------
 
 
-# execute the main function 
+# Execute the main function 
 if __name__ == '__main__':
     main()
