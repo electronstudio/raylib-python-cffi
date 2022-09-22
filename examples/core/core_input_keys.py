078f185b6b04022ac1056f591ecad4a9303f76b0
דור שפירא <Dor.Shapira@outlook.com>
2022-09-22 16:42:32 +0300

    make core_input_keys.py stand for the conventions

--- examples/core/core_input_keys.py
+++ examples/core/core_input_keys.py
@@ -3,48 +3,65 @@
 raylib [core] example - Keyboard input
 
 """
-import pyray
+
+# Import
+# ------------------------------------------------------------------------------------
+from pyray import *
 from raylib.colors import (
     RAYWHITE,
     DARKGRAY,
     MAROON,
 )
+# ------------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------------
+# Program main entry point
+# ------------------------------------------------------------------------------------
+def main():
+    # Initialization
+    # ----------------------------------------------------------------------------------
+    screenWidth = 800
+    screenHeight = 450
+
+    init_window(screenWidth, screenHeight, "raylib [core] example - keyboard input")
+
+    ball_position = Vector2(screenWidth / 2, screenHeight / 2)
+
+    set_target_fps(60)  # Set our game to run at 60 frames-per-second
+    # ----------------------------------------------------------------------------------
+
+    # Main game loop
+    while not window_should_close():  # Detect window close button or ESC key
+        # Update
+        # ----------------------------------------------------------------------------------
+        if is_key_down(KeyboardKey.KEY_RIGHT):
+            ball_position.x += 2
+        if is_key_down(KeyboardKey.KEY_LEFT):
+            ball_position.x -= 2
+        if is_key_down(KeyboardKey.KEY_UP):
+            ball_position.y -= 2
+        if is_key_down(KeyboardKey.KEY_DOWN):
+            ball_position.y += 2
+        # ----------------------------------------------------------------------------------
+
+        # Draw
+        # ----------------------------------------------------------------------------------
+        begin_drawing()
+
+        clear_background(RAYWHITE)
+        draw_text("move the ball with arrow keys", 10, 10, 20, DARKGRAY)
+        draw_circle_v(ball_position, 50, MAROON)
+
+        end_drawing()
+        # ----------------------------------------------------------------------------------
+
+    # De-Initialization
+    # ----------------------------------------------------------------------------------
+    close_window()  # Close window and OpenGL context
+    # ----------------------------------------------------------------------------------
 
 
+# execute the main function
+if __name__ == '__main__':
+    main()
 
-
-# Initialization
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 450
-
-pyray.init_window(SCREEN_WIDTH, SCREEN_HEIGHT,
-                  'raylib [core] example - keyboard input')
-ball_position = pyray.Vector2(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-
-pyray.set_target_fps(60)  # Set our game to run at 60 frames-per-second
-
-
-# Main game loop
-while not pyray.window_should_close():  # Detect window close button or ESC key
-    # Update
-    if pyray.is_key_down(pyray.KEY_RIGHT):
-        ball_position.x += 2
-    if pyray.is_key_down(pyray.KEY_LEFT):
-        ball_position.x -= 2
-    if pyray.is_key_down(pyray.KEY_UP):
-        ball_position.y -= 2
-    if pyray.is_key_down(pyray.KEY_DOWN):
-        ball_position.y += 2
-
-    # Draw
-    pyray.begin_drawing()
-
-    pyray.clear_background(RAYWHITE)
-    pyray.draw_text('move the ball with arrow keys', 10, 10, 20, DARKGRAY)
-    pyray.draw_circle_v(ball_position, 50, MAROON)
-
-    pyray.end_drawing()
-
-
-# De-Initialization
-pyray.close_window()  # Close window and OpenGL context
