46cac6b7c2cffbf02320e79a0b8d127375c37880
דור שפירא <Dor.Shapira@outlook.com>
2022-09-22 16:23:46 +0300

    make shapes_logo_raylib.py stand for the conventions

--- examples/shapes/shapes_logo_raylib.py
+++ examples/shapes/shapes_logo_raylib.py
@@ -3,41 +3,60 @@
 raylib [shapes] example - Logo Raylib
 
 """
+
+# Import
+# ------------------------------------------------------------------------------------
 from pyray import *
 from raylib.colors import (
     RAYWHITE,
     BLACK,
     GRAY
 )
+# ------------------------------------------------------------------------------------
 
-# Initialization
-screenWidth = 800
-screenHeight = 450
-
-init_window(screenWidth, screenHeight, 'raylib [shapes] example - raylib logo using shapes')
-
-set_target_fps(60)  # Set our game to run at 60 frames-per-second
-
-# Main game loop
-while not window_should_close():  # Detect window close button or ESC key
-    # Update
+# ------------------------------------------------------------------------------------
+# Program main entry point
+# ------------------------------------------------------------------------------------
+def main():
+    # Initialization
     # ----------------------------------------------------------------------------------
-    # TODO: Update your variables here
+    screenWidth = 800
+    screenHeight = 450
+
+    init_window(screenWidth, screenHeight, 'raylib [shapes] example - raylib logo using shapes')
+
+    set_target_fps(60)  # Set our game to run at 60 frames-per-second
     # ----------------------------------------------------------------------------------
 
-    # Draw
+    # Main game loop
+    while not window_should_close():  # Detect window close button or ESC key
+        # Update
+        # ----------------------------------------------------------------------------------
+        # TODO: Update your variables here
+        # ----------------------------------------------------------------------------------
+
+        # Draw
+        # ----------------------------------------------------------------------------------
+        begin_drawing()
+
+        clear_background(RAYWHITE)
+
+        draw_rectangle(int(screenWidth/2 - 128), int(screenHeight/2 - 128), 256, 256, BLACK)
+        draw_rectangle(int(screenWidth/2 - 112), int(screenHeight/2 - 112), 224, 224, RAYWHITE)
+        draw_text("raylib", int(screenWidth/2 - 44), int(screenHeight/2 + 48), 50, BLACK)
+
+        draw_text("this is NOT a texture!", 350, 370, 10, GRAY)
+
+        end_drawing()
+        # ----------------------------------------------------------------------------------
+
+    # De-Initialization
     # ----------------------------------------------------------------------------------
-    begin_drawing()
+    close_window()  # Close window and OpenGL context
+    # ----------------------------------------------------------------------------------
 
-    clear_background(RAYWHITE)
 
-    draw_rectangle(int(screenWidth/2 - 128), int(screenHeight/2 - 128), 256, 256, BLACK)
-    draw_rectangle(int(screenWidth/2 - 112), int(screenHeight/2 - 112), 224, 224, RAYWHITE)
-    draw_text("raylib", int(screenWidth/2 - 44), int(screenHeight/2 + 48), 50, BLACK)
+# execute the main function
+if __name__ == '__main__':
+    main()
 
-    draw_text("this is NOT a texture!", 350, 370, 10, GRAY)
-
-    end_drawing()
-
-# De-Initialization
-close_window()  # Close window and OpenGL context
