3a4b90c8f750881215ba3e8f5bab4fc8d3d6b7ee
Dor Shapira <107134807+sDos280@users.noreply.github.com>
2022-09-22 21:23:15 +0300

    Update shapes_logo_raylib.py

--- examples/shapes/shapes_logo_raylib.py
+++ examples/shapes/shapes_logo_raylib.py
@@ -56,7 +56,7 @@ def main():
     # ----------------------------------------------------------------------------------
 
 
-# execute the main function
+# Execute the main function
 if __name__ == '__main__':
     main()
 
