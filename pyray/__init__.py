2c233d47faf55115f660f9a7b80412b927cb77fb
richard <github@fantastic.me.uk>
2022-09-23 20:11:59 +0100

    reformat fix https://github.com/electronstudio/raylib-python-cffi/issues/90

--- pyray/__init__.py
+++ pyray/__init__.py
@@ -14,23 +14,20 @@
 
 from raylib import rl, ffi
 from raylib.colors import *
+
 try:
     from raylib.defines import *
 except AttributeError:
     print("sorry deprecated enums dont work on dynamic version")
 
-
-from inspect import ismethod,getmembers,isbuiltin
+from inspect import getmembers, isbuiltin
 import inflection
 
 current_module = __import__(__name__)
 
 
-
 def pointer(self, struct):
     return ffi.addressof(struct)
-
-
 
 
 # I'm concerned that we are doing a lot of string comparisons on every function call to detect types.
@@ -42,11 +39,11 @@ def pointer(self, struct):
 # Another way to improve performance might be to special-case simple types before doing the string comparisons
 
 def makefunc(a):
-    #print("makefunc ",a, ffi.typeof(a).args)
+    # print("makefunc ",a, ffi.typeof(a).args)
     def func(*args):
         modified_args = []
         for (c_arg, arg) in zip(ffi.typeof(a).args, args):
-            #print("arg:",str(arg), "c_arg.kind:", c_arg.kind, "c_arg:", c_arg, "type(arg):",str(type(arg)))
+            # print("arg:",str(arg), "c_arg.kind:", c_arg.kind, "c_arg:", c_arg, "type(arg):",str(type(arg)))
             if c_arg.kind == 'pointer':
                 if type(arg) == str:
                     arg = arg.encode('utf-8')
@@ -56,9 +53,9 @@ def makefunc(a):
                     arg = ffi.new("int *", arg)
                 elif type(arg) is float:
                     arg = ffi.new("float *", arg)
-                elif str(type(arg)) == "<class '_cffi_backend.__CDataOwn'>" and "*" not in str(arg): #CPython
+                elif str(type(arg)) == "<class '_cffi_backend.__CDataOwn'>" and "*" not in str(arg):  # CPython
                     arg = ffi.addressof(arg)
-                elif str(type(arg)) == "<class '_cffi_backend._CDataBase'>" and "*" not in str(arg): #Pypy
+                elif str(type(arg)) == "<class '_cffi_backend._CDataBase'>" and "*" not in str(arg):  # Pypy
                     arg = ffi.addressof(arg)
                 elif arg is None:
                     arg = ffi.NULL
@@ -72,28 +69,32 @@ def makefunc(a):
             else:
                 result = ffi.string(result).decode('utf-8')
         return result
+
     return func
+
 
 def makeStructHelper(struct):
     def func(*args):
         return ffi.new(f"struct {struct} *", args)[0]
+
     return func
 
 
 for name, attr in getmembers(rl):
-    #print(name, attr)
-    uname = inflection.underscore(name).replace('3_d','_3d').replace('2_d','_2d')
-    if isbuiltin(attr) or str(type(attr)) == "<class '_cffi_backend.__FFIFunctionWrapper'>" or str(type(attr)) == "<class '_cffi_backend._CDataBase'>":
-        #print(attr.__call__)
-        #print(attr.__doc__)
-        #print(attr.__text_signature__)
-        #print(dir(attr))
-        #print(dir(attr.__repr__))
+    # print(name, attr)
+    uname = inflection.underscore(name).replace('3_d', '_3d').replace('2_d', '_2d')
+    if isbuiltin(attr) or str(type(attr)) == "<class '_cffi_backend.__FFIFunctionWrapper'>" or str(
+            type(attr)) == "<class '_cffi_backend._CDataBase'>":
+        # print(attr.__call__)
+        # print(attr.__doc__)
+        # print(attr.__text_signature__)
+        # print(dir(attr))
+        # print(dir(attr.__repr__))
         f = makefunc(attr)
         setattr(current_module, uname, f)
-        #def wrap(*args):
+        # def wrap(*args):
         #    print("call to ",attr)
-        #setattr(PyRay, uname, lambda *args: print("call to ",attr))
+        # setattr(PyRay, uname, lambda *args: print("call to ",attr))
     else:
         setattr(current_module, name, attr)
 
