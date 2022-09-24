9fffa31885800d721b7bcdf79341ca00f6595e1a
דור שפירא <Dor.Shapira@outlook.com>
2022-09-24 14:51:36 +0300

    making shapes_following_eyes.py stand for the Python API conventions

--- examples/shapes/shapes_following_eyes.py
+++ examples/shapes/shapes_following_eyes.py
@@ -27,18 +27,18 @@ from math import (
 def main():
     # Initialization
     # ----------------------------------------------------------------------------------
-    screenWidth = 800
-    screenHeight = 450
+    SCREEN_WIDTH = 800
+    SCREEN_HEIGHT = 450
 
-    init_window(screenWidth, screenHeight, "raylib [shapes] example - following eyes")
+    init_window(SCREEN_WIDTH, SCREEN_HEIGHT, "raylib [shapes] example - following eyes")
 
-    scleraLeftPosition = Vector2(screenWidth / 2.0 - 100.0, screenHeight / 2.0)
-    scleraRightPosition = Vector2(screenWidth / 2.0 + 100.0, screenHeight / 2.0)
-    scleraRadius = 80.0
+    scleraLeftPosition = Vector2(SCREEN_WIDTH / 2.0 - 100.0, SCREEN_HEIGHT / 2.0)
+    scleraRightPosition = Vector2(SCREEN_WIDTH / 2.0 + 100.0, SCREEN_HEIGHT / 2.0)
+    sclera_radius = 80.0
 
-    irisLeftPosition = Vector2(screenWidth / 2.0 - 100.0, screenHeight / 2.0)
-    irisRightPosition = Vector2(screenWidth / 2.0 - 100.0, screenHeight / 2.0)
-    irisRadius = 24.0
+    irisLeftPosition = Vector2(SCREEN_WIDTH / 2.0 - 100.0, SCREEN_HEIGHT / 2.0)
+    irisRightPosition = Vector2(SCREEN_WIDTH / 2.0 - 100.0, SCREEN_HEIGHT / 2.0)
+    iris_radius = 24.0
 
     angle = 0.0
     dx, dy, dxx, dyy = 0.0, 0.0, 0.0, 0.0
@@ -54,27 +54,27 @@ def main():
         irisRightPosition = get_mouse_position()
 
         # Check not inside the left eye sclera
-        if not check_collision_point_circle(irisLeftPosition, scleraLeftPosition, scleraRadius - 20):
+        if not check_collision_point_circle(irisLeftPosition, scleraLeftPosition, sclera_radius - 20):
             dx = irisLeftPosition.x - scleraLeftPosition.x
             dy = irisLeftPosition.y - scleraLeftPosition.y
 
             angle = atan2(dy, dx)
 
-            dxx = (scleraRadius - irisRadius)*cos(angle)
-            dyy = (scleraRadius - irisRadius)*sin(angle)
+            dxx = (sclera_radius - iris_radius) * cos(angle)
+            dyy = (sclera_radius - iris_radius) * sin(angle)
 
             irisLeftPosition.x = scleraLeftPosition.x + dxx
             irisLeftPosition.y = scleraLeftPosition.y + dyy
 
         # Check not inside the right eye sclera
-        if not check_collision_point_circle(irisRightPosition, scleraRightPosition, scleraRadius - 20):
+        if not check_collision_point_circle(irisRightPosition, scleraRightPosition, sclera_radius - 20):
             dx = irisRightPosition.x - scleraRightPosition.x
             dy = irisRightPosition.y - scleraRightPosition.y
 
             angle = atan2(dy, dx)
 
-            dxx = (scleraRadius - irisRadius)*cos(angle)
-            dyy = (scleraRadius - irisRadius)*sin(angle)
+            dxx = (sclera_radius - iris_radius) * cos(angle)
+            dyy = (sclera_radius - iris_radius) * sin(angle)
 
             irisRightPosition.x = scleraRightPosition.x + dxx
             irisRightPosition.y = scleraRightPosition.y + dyy
@@ -86,12 +86,12 @@ def main():
 
         clear_background(RAYWHITE)
 
-        draw_circle_v(scleraLeftPosition, scleraRadius, LIGHTGRAY)
-        draw_circle_v(irisLeftPosition, irisRadius, BROWN)
+        draw_circle_v(scleraLeftPosition, sclera_radius, LIGHTGRAY)
+        draw_circle_v(irisLeftPosition, iris_radius, BROWN)
         draw_circle_v(irisLeftPosition, 10, BLACK)
 
-        draw_circle_v(scleraRightPosition, scleraRadius, LIGHTGRAY)
-        draw_circle_v(irisRightPosition, irisRadius, DARKGREEN)
+        draw_circle_v(scleraRightPosition, sclera_radius, LIGHTGRAY)
+        draw_circle_v(irisRightPosition, iris_radius, DARKGREEN)
         draw_circle_v(irisRightPosition, 10, BLACK)
 
         draw_fps(10, 10)
