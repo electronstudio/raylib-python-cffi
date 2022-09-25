0939ca3a0692a39d4d6983c8b65fbd1cd9e330f0
דור שפירא <Dor.Shapira@outlook.com>
2022-09-25 08:43:59 +0300

    made core_window_should_close.py stand for the Python API conventions

--- examples/core/core_window_should_close.py
+++ examples/core/core_window_should_close.py
@@ -7,18 +7,6 @@ raylib [core] example - Window Should Close
 # Import
 # ------------------------------------------------------------------------------------
 from pyray import *
-from raylib.colors import (
-    RAYWHITE,
-    LIGHTGRAY,
-    WHITE,
-    BLACK
-)
-from raylib import (
-    KEY_NULL,
-    KEY_ESCAPE,
-    KEY_Y,
-    KEY_N
-)
 # ------------------------------------------------------------------------------------
 
 # ------------------------------------------------------------------------------------
@@ -27,34 +15,34 @@ from raylib import (
 def main():
     # Initialization
     # --------------------------------------------------------------------------------------
-    screenWidth = 800
-    screenHeight = 450
+    SCREEN_WIDTH = 800
+    SCREEN_HEIGHT = 450
 
-    init_window(screenWidth, screenHeight, "raylib [core] example - window should close")
+    init_window(SCREEN_WIDTH, SCREEN_HEIGHT, "raylib [core] example - window should close")
 
-    set_exit_key(KEY_NULL)  # Disable KEY_ESCAPE to close window, X-button still works
+    set_exit_key(KeyboardKey.KEY_NULL)  # Disable KEY_ESCAPE to close window, X-button still works
 
-    exitWindowRequested = False  # Flag to request window to exit
-    exitWindow = False  # Flag to set window to exit
+    exit_window_requested = False  # Flag to request window to exit
+    exit_window = False  # Flag to set window to exit
 
     set_target_fps(60)  # Set our game to run at 60 frames-per-second
     # --------------------------------------------------------------------------------------
 
     # Main game loop
-    while not exitWindow:
+    while not exit_window:
         # Update
         # ----------------------------------------------------------------------------------
         #  Detect if X-button or KEY_ESCAPE have been pressed to close window
-        if window_should_close() or is_key_pressed(KEY_ESCAPE):
-            exitWindowRequested = True
+        if window_should_close() or is_key_pressed(KeyboardKey.KEY_ESCAPE):
+            exit_window_requested = True
 
-        if exitWindowRequested:
+        if exit_window_requested:
             # A request for close window has been issued, we can save data before closing
             # or just show a message asking for confirmation
-            if is_key_pressed(KEY_Y):
-                exitWindow = True
-            elif is_key_pressed(KEY_N):
-                exitWindowRequested = False
+            if is_key_pressed(KeyboardKey.KEY_Y):
+                exit_window = True
+            elif is_key_pressed(KeyboardKey.KEY_N):
+                exit_window_requested = False
         # ----------------------------------------------------------------------------------
 
         # Draw
@@ -63,8 +51,8 @@ def main():
 
         clear_background(RAYWHITE)
 
-        if exitWindowRequested:
-            draw_rectangle(0, 100, screenWidth, 200, BLACK)
+        if exit_window_requested:
+            draw_rectangle(0, 100, SCREEN_WIDTH, 200, BLACK)
             draw_text("Are you sure you want to exit program? [Y/N]", 40, 180, 30, WHITE)
 
         else:
