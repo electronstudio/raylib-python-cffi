0a677fe31d1a6fcd3e488f7c206c86745114ae01
Richard Smith <github@electronstudio.co.uk>
2024-04-05 12:47:38 +0100

    fix float pointer test

--- tests/test_float_pointers.py
+++ tests/test_float_pointers.py
@@ -1,4 +1,5 @@
 import pyray as pr
+import pytest
 
 pr.init_window(1280, 720, "any")
 
@@ -6,4 +7,5 @@ shader = pr.load_shader("", "shader.fs")
 time = pr.ffi.new("float *", 0.0)
 timeLoc = pr.get_shader_location(shader, "uTime")
 pr.set_shader_value(shader, timeLoc, time, pr.SHADER_UNIFORM_FLOAT)
-pr.set_shader_value(shader, timeLoc, 0.0, pr.SHADER_UNIFORM_FLOAT)
+with pytest.raises(TypeError):
+    pr.set_shader_value(shader, timeLoc, 0.0, pr.SHADER_UNIFORM_FLOAT)
