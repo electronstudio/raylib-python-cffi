16df0581c973e58083e7c73d4462331449ee0d2e
Richard Smith <github@electronstudio.co.uk>
2024-06-09 23:04:41 +0100

    type hints for colors

--- pyray/__init__.pyi
+++ pyray/__init__.pyi
@@ -2344,10 +2344,10 @@ def glfw_get_required_instance_extensions(count: Any,) -> list[str]:
 def glfw_get_time() -> float:
         """"""
         ...
-def glfw_get_timer_frequency() -> uint64_t:
+def glfw_get_timer_frequency() -> int:
         """"""
         ...
-def glfw_get_timer_value() -> uint64_t:
+def glfw_get_timer_value() -> int:
         """"""
         ...
 def glfw_get_version(major: Any,minor: Any,rev: Any,) -> None:
@@ -3397,6 +3397,34 @@ class rlVertexBuffer:
         self.indices=indices
         self.vaoId=vaoId
         self.vboId=vboId
+
+LIGHTGRAY  : Color
+GRAY       : Color
+DARKGRAY   : Color
+YELLOW     : Color
+GOLD       : Color
+ORANGE     : Color
+PINK       : Color
+RED        : Color
+MAROON     : Color
+GREEN      : Color
+LIME       : Color
+DARKGREEN  : Color
+SKYBLUE    : Color
+BLUE       : Color
+DARKBLUE   : Color
+PURPLE     : Color
+VIOLET     : Color
+DARKPURPLE : Color
+BEIGE      : Color
+BROWN      : Color
+DARKBROWN  : Color
+WHITE      : Color
+BLACK      : Color
+BLANK      : Color
+MAGENTA    : Color
+RAYWHITE   : Color
+
 from enum import IntEnum
 
 class ConfigFlags(IntEnum):
@@ -4110,45 +4138,4 @@ class GuiIconName(IntEnum):
     ICON_253 = 253
     ICON_254 = 254
     ICON_255 = 255
-
-#  Copyright (c) 2021 Richard Smith and others
-#
-#  This program and the accompanying materials are made available under the
-#  terms of the Eclipse Public License 2.0 which is available at
-#  http://www.eclipse.org/legal/epl-2.0.
-#
-#  This Source Code may also be made available under the following Secondary
-#  licenses when the conditions for such availability set forth in the Eclipse
-#  Public License, v. 2.0 are satisfied: GNU General Public License, version 2
-#  with the GNU Classpath Exception which is
-#  available at https://www.gnu.org/software/classpath/license.html.
-#
-#  SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
-
-LIGHTGRAY  =( 200, 200, 200, 255 )
-GRAY       =( 130, 130, 130, 255 )
-DARKGRAY   =( 80, 80, 80, 255 )
-YELLOW     =( 253, 249, 0, 255 )
-GOLD       =( 255, 203, 0, 255 )
-ORANGE     =( 255, 161, 0, 255 )
-PINK       =( 255, 109, 194, 255 )
-RED        =( 230, 41, 55, 255 )
-MAROON     =( 190, 33, 55, 255 )
-GREEN      =( 0, 228, 48, 255 )
-LIME       =( 0, 158, 47, 255 )
-DARKGREEN  =( 0, 117, 44, 255 )
-SKYBLUE    =( 102, 191, 255, 255 )
-BLUE       =( 0, 121, 241, 255 )
-DARKBLUE   =( 0, 82, 172, 255 )
-PURPLE     =( 200, 122, 255, 255 )
-VIOLET     =( 135, 60, 190, 255 )
-DARKPURPLE =( 112, 31, 126, 255 )
-BEIGE      =( 211, 176, 131, 255 )
-BROWN      =( 127, 106, 79, 255 )
-DARKBROWN  =( 76, 63, 47, 255 )
-WHITE      =( 255, 255, 255, 255 )
-BLACK      =( 0, 0, 0, 255 )
-BLANK      =( 0, 0, 0, 0 )
-MAGENTA    =( 255, 0, 255, 255 )
-RAYWHITE   =( 245, 245, 245, 255 )
 
